384848857f702546afae92ab50791a6e760d3d8f
SxovaZeroTree <xmasimbe965@gmail.com>
2026-06-30 20:23:42 +0200

    New firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,17 +2,22 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAn5QFh6RjOxXeFplA6MRejmWHyHlll87c",
+  authDomain: "the-glorious-church.firebaseapp.com",
+  projectId: "the-glorious-church",
+  storageBucket: "the-glorious-church.firebasestorage.app",
+  messagingSenderId: "262634622804",
+  appId: "1:262634622804:web:230d7750f52fa63f2582c1",
+  measurementId: "G-609S19MN7Q"
 };
+
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
